f28170dd6ba2e6a54ca3c7f2bc21861bb0c5b5f2
Kun Xi <kunxi@kunxi.org>
2022-01-15 22:35:51 -0800

    Split git module.

---
+++ git/default.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+
+    userName = "Kun Xi";
+    userEmail = "kunxi@kunxi.org";
+    aliases = {
+      a = "add";
+      br = "branch";
+      st = "status";
+      ss = "status --short";
+      ci = "commit";
+      di = "diff";
+      co = "checkout";
+      w = "whatchanged";
+      vimdiff = "difftool --tool=vimdiff -y";
+      vd = "difftool --tool=vimdiff -y";
+      od = "difftool --tool=opendiff -y";
+      unstage = "reset HEAD";
+      lol = "log --oneline --graph --decorate";
+      hist = "log --all --graph --pretty='[%C(cyan)%h%Creset]%C(bold cyan)%d%Creset %s'";
+      showdiff = "!sh -c 'git difftool --tool=vimdiff -y $1^ $1' -";
+      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      wl = "walklog";
+      dt = "difftool";
+    };
+
+    extraConfig = {
+      core = {
+        editor = "nvim";
+        preloadingindex = true;
+      };
+
+      color = {
+        diff = "auto";
+        status = "auto";
+        branch = "auto";
+        interactive = "auto";
+        ui = true;
+        pager = true;
+      };
+
+      merge.tool = "vimdiff";
+      diff.tool = "vimdiff";
+      difftool.prompt = false;
+      pull.rebaes = true;
+      push.default = "current";
+    };
+
+    # TODO: add gitignore.
+  };
+}
