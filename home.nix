7314536598a00dfb9d31aba95f61fae9a5158b2b
Kun Xi <kunxi@kunxi.org>
2022-01-16 00:35:45 -0800

    Split tmux module.

--- home.nix
+++ home.nix
@@ -33,7 +33,6 @@ in
       VISUAL = "nvim";
       LANG = locale;
       LC_ALL = locale;
-      TMUX_TMPDIR = "/tmp";
     };
 
     username = username;
@@ -42,24 +41,6 @@ in
   };
 
   programs.home-manager.enable = true;
-
-  # tmux
-  programs.tmux = {
-    enable = true;
-    terminal = "screen-256color";
-    shortcut = "a";
-    baseIndex = 1;
-    keyMode = "vi";
-    sensibleOnTop  = true;
-    plugins = with pkgs; [
-      tmuxPlugins.pain-control
-      tmuxPlugins.copycat
-      tmuxPlugins.yank
-      tmuxPlugins.logging
-    ];
-
-    # TODO: still need the configure the theme for eye-candy.
-  };
 
   # Use nix-direnv integration
   programs.direnv = {
---
+++ tmux/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    shortcut = "a";
+    baseIndex = 1;
+    keyMode = "vi";
+    sensibleOnTop  = true;
+    plugins = with pkgs; [
+      tmuxPlugins.pain-control
+      tmuxPlugins.copycat
+      tmuxPlugins.yank
+      tmuxPlugins.logging
+    ];
+
+    # TODO: still need the configure the theme for eye-candy.
+  };
+
+  home.sessionVariables = {
+    TMUX_TMPDIR = "/tmp";
+  };
+}
