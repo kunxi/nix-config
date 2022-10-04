7a0d842695b70d3ec59d3ae3dffe3263c1e2dff7
Kun Xi <kunxi@kunxi.org>
2022-10-04 06:47:33 -0700

    Support both WSL and macOS (#1)
    * Add more tools from lucperkins/nix-home-config.
    
    * Consolidate devops to home.
    
    * Consolidate more packages.

--- home.nix
+++ home.nix
@@ -1,59 +1,126 @@
 { config, pkgs, ... }:
 
 let
-  locale = "C.UTF-8";
+  locale = "en_US.UTF-8";
   homedir = builtins.getEnv "HOME";
   username = builtins.getEnv "USER";
+
+  devopsPackages = with pkgs; [
+    awscli
+    aws-sam-cli
+    aws-iam-authenticator
+
+    kubectl
+    kubectx
+    kubernetes-helm
+    skaffold
+    tilt
+
+    terraform
+    vault
+  ];
+
+  pythonPackages = with pkgs.python310Packages; [
+    black
+    cookiecutter
+    flake8
+    ipython
+    pip
+    python
+    setuptools
+
+    # python, if we want to use conda
+    # First time use:
+    # $ conda-shell
+    # $ conda-install
+    # Normal use:
+    # $ conda-shell
+    # $ conda install ipython numpy
+    # conda
+  ];
+
 in
 {
   imports = [
-    ./cli.nix
-    ./fzf.nix
     ./git.nix
     ./neovim.nix
     ./zsh.nix
     ./rust.nix
-    ./cloud.nix
-    ./python.nix
     ./nodejs/default.nix
     ./minizinc/default.nix
     ./tmux.nix
-    ./zoxide.nix
   ];
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnsupportedSystem = true;
+  };
+
+  programs.home-manager.enable = true;
+  
   home = {
-    packages = with pkgs; [
-      datamash
-      dig
-      dos2unix
-      file
-      gcc
-      gnumake
-      graphviz
-      htop
-      inetutils
-      ltrace
-      openssh
-      patch
-      rlwrap  # rlwrap sqlite3 foo.db for readline support.
-      sqlite
-      strace
-      wget
-      unzip
-      # TODO: add gm, ffmpeg with CUDA 
-    ];
-
-    sessionVariables = {
-      LANG = locale;
-      LC_ALL = locale;
-    };
-
     username = username;
     homeDirectory = homedir;
     stateVersion = "22.11";
   };
 
-  programs.home-manager.enable = true;
+  home.sessionVariables = {
+      LANG = locale;
+      LC_ALL = locale;
+  };
+
+    home.packages = with pkgs; [
+    # dig
+    # gcc
+    # gnumake
+    # inetutils
+    #ltrace
+    # strace
+    autoconf
+    bat # cat replacement written in Rust
+    curl
+    datamash
+    direnv
+    docker
+    docker-compose
+    dos2unix
+    exa
+    fd
+    file
+    fzf
+    graphviz
+    gradle
+    htop
+    httpie
+    jq
+    just
+    mdcat
+    neofetch
+    nomad
+    openssh
+    patch
+    parallel
+    podman
+    pre-commit
+    redis
+    ripgrep
+    rlwrap  # rlwrap sqlite3 foo.db for readline support.
+    sd # sed replacement
+    skim # fuzzy finder
+    sqlite
+    starship
+    tealdeer
+    tokei # Handy tool to see lines of code by language
+    tree
+    unzip
+    vscode
+    wget
+    xsv
+    yarn
+    zoxide
+    # TODO: add gm, ffmpeg with CUDA
+  ] ++ devopsPackages ++ pythonPackages;
+
 
   # Use nix-direnv integration
   programs.direnv = {
