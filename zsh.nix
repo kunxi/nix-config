bc63177abade90dbcca2cb0cb09c375390f7faaf
Kun Xi <kunxi@kunxi.org>
2022-10-04 17:15:46 -0700

    Enable enhancd with zsh plugin.

--- zsh.nix
+++ zsh.nix
@@ -87,10 +87,14 @@
 
   };
 
-  programs.zsh.zplug = {
-    enable = false;
-    plugins = [
-      { name = "b4b4r07/enhancd"; tags = [use:init.sh]; }
-    ];
-  };
+  programs.zsh.plugins = [{
+    name = "enhancd";
+    file = "init.sh";
+    src = pkgs.fetchFromGitHub {
+      owner = "b4b4r07";
+      repo = "enhancd";
+      rev = "v2.2.1";
+      sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
+    };
+  }];
 }
