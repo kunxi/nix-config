21972a60b2d6b119b4cdffbb70488323fe819365
Kun Xi <kunxi@kunxi.org>
2023-02-28 22:18:24 -0800

    Add pass, gpg.

---
+++ crypto.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    gnupg
+    pass
+    pinentry
+  ];
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = false;
+    pinentryFlavor = "curses";
+  };
+
+  # NO need to setup GPG_TTY, the gpg module of prezto handles it.
+  # TODO: setup SSH_AUTH_SOCK and use gpg-agent to manage ssh sessions.
+}
+
